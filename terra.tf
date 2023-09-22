4fd091c7f37c0f29ccf7256f382c8e5970086764
konark111 <konarkwalia94@gmail.com>
2023-09-22 07:36:34 +0000

    add .gitignore and update config

--- terra.tf
+++ terra.tf
@@ -1,35 +1,89 @@
 # main.tf
 
-provider "aws" {
-  region = "us-east-1"
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "vpc-terra"
+  }
 }
 
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+resource "aws_security_group" "ssh-access" {
+  name   = "ssh-access"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+# create prublic subnet in az1
+resource "aws_subnet" "public_subnet_az1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "Publicsubnet_az1"
+  }
 }
 
-resource "aws_subnet" "example" {
-  vpc_id     = aws_vpc.example.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name = "public_RT"
+  }
 }
+resource "aws_route_table_association" "public_subnet_az1_rt_association" {
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_instance" "server1" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.web.key_name
+  subnet_id              = aws_subnet.public_subnet_az1.id
+  vpc_security_group_ids = [aws_security_group.ssh-access.id]
+  #user_data              = file("ins_script.sh")
+  #  count = 5
+  tags = {
+    Name = "public-ec2"
+    #    Name = "Public-jump-${format("%02d", count.index + 1)}"
+  }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0261755bbcb8c4a84"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.example.id
 }
-resource "aws_instance" "example2" {
-  ami           = "ami-0261755bbcb8c4a84"
-  instance_type = "t2.medium"
-  subnet_id     = aws_subnet.example.id
+resource "aws_key_pair" "web" {
+  key_name   = "id_rsa"
+  public_key = file("./id_rsa.pub")
 }
-
-resource "aws_instance" "example3tf" {
-  ami           = "ami-0261755bbcb8c4a84"
-  instance_type = "t2.medium"
-  subnet_id     = aws_subnet.example.id
-}
-
 resource "aws_s3_bucket" "tf-s3" {
   bucket = "s3-tfstate-6969"
   acl    = "private"
